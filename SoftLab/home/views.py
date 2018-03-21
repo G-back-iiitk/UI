4f8b1203599fd50bab7dd7c5b39e3b1352ecf58f
vikrant <vikrantkarn007@gmail.com>
2018-03-21 19:21:48 +0530

    Need to fix static file

--- SoftLab/home/views.py
+++ SoftLab/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'home/index.html')
+    Stock_name =['SnP 500','Tesla','Boring Co.','Genral Motors', 'Tata Steels']
+    Stock_price =['300','400','250','800','600']
+    Stock = zip(Stock_name,Stock_price)
+    return render(request,'home/index.html',{'stocks':Stock})
