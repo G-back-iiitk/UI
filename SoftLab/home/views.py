7476c13ffc4e1a268602dbd7b7c37c3db9ef4860
mevk_7 <vikrantkarn007@gmail.com>
2018-03-23 19:41:19 +0530

    Refator Templates

--- SoftLab/home/views.py
+++ SoftLab/home/views.py
@@ -5,4 +5,10 @@ def index(request):
     Stock_name =['SnP 500','Tesla','Boring Co.','Genral Motors', 'Tata Steels']
     Stock_price =['300','400','250','800','600']
     Stock = zip(Stock_name,Stock_price)
+
+    if (request.method == 'POST'):
+    	print(request.POST['select'])
+    	return render(request,'home/show.html')
+
+
     return render(request,'home/index.html',{'stocks':Stock})
